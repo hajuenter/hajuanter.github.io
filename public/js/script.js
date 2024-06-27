b84f02e25fd70631ea9694e874dabbeb9e7b1235
TheHajuenter <150408093+TheHajuenter@users.noreply.github.com>
2024-06-27 13:32:23 +0700

    final

--- public/js/script.js
+++ public/js/script.js
@@ -27,54 +27,54 @@ window.onscroll = function() {
     }
 };
 
-// document.addEventListener('DOMContentLoaded', (event) => {
-//     const audioFiles = [
-//         './audio/musik1.mp3',
-//         './audio/musik2.mp3',
-//         './audio/musik3.mp3',
-//         './audio/musik4.mp3',
-//         './audio/musik5.mp3',
-//         './audio/musik7.mp3',
-//         './audio/musik8.mp3',
-//         './audio/musik9.mp3',
-//         './audio/musik10.mp3',
-//         './audio/musik11.mp3',
-//         './audio/musik12.mp3'
-//     ];
+document.addEventListener('DOMContentLoaded', (event) => {
+    const audioFiles = [
+        './audio/musik1.mp3',
+        './audio/musik2.mp3',
+        './audio/musik3.mp3',
+        './audio/musik4.mp3',
+        './audio/musik5.mp3',
+        './audio/musik7.mp3',
+        './audio/musik8.mp3',
+        './audio/musik9.mp3',
+        './audio/musik10.mp3',
+        './audio/musik11.mp3',
+        './audio/musik12.mp3'
+    ];
 
-//     let currentAudioIndex = 0;
-//     const audioPlayer = document.getElementById('audioPlayer');
-//     const pausePlayButton = document.getElementById('pausePlayButton');
-//     // Function to load and play the next audio file
-//     const playNextAudio = () => {
-//         if (currentAudioIndex < audioFiles.length) {
-//             audioPlayer.src = audioFiles[currentAudioIndex];
-//             audioPlayer.play();
-//             currentAudioIndex++;
-//         } else {
-//             // Reset to the first audio file after all files have been played
-//             currentAudioIndex = 0;
-//             playNextAudio();
-//         }
-//     };
+    let currentAudioIndex = 0;
+    const audioPlayer = document.getElementById('audioPlayer');
+    const pausePlayButton = document.getElementById('pausePlayButton');
+    // Function to load and play the next audio file
+    const playNextAudio = () => {
+        if (currentAudioIndex < audioFiles.length) {
+            audioPlayer.src = audioFiles[currentAudioIndex];
+            audioPlayer.play();
+            currentAudioIndex++;
+        } else {
+            // Reset to the first audio file after all files have been played
+            currentAudioIndex = 0;
+            playNextAudio();
+        }
+    };
 
-//     // Event listener to play the next audio file when the current one ends
-//     audioPlayer.addEventListener('ended', playNextAudio);
+    // Event listener to play the next audio file when the current one ends
+    audioPlayer.addEventListener('ended', playNextAudio);
 
-//     // Start playing the first audio file
-//     playNextAudio();
+    // Start playing the first audio file
+    playNextAudio();
 
-// pausePlayButton.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     if (audioPlayer.paused) {
-//         audioPlayer.play();
-//         pausePlayButton.textContent = 'BM';
-//     } else {
-//         audioPlayer.pause();
-//         pausePlayButton.textContent = 'BM';
-//     }
-// });
-// });
+pausePlayButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (audioPlayer.paused) {
+        audioPlayer.play();
+        pausePlayButton.textContent = 'BM';
+    } else {
+        audioPlayer.pause();
+        pausePlayButton.textContent = 'BM';
+    }
+});
+});
 
 //dark mode toggle
 const darkToggle = document.querySelector('#dark-toggle');
